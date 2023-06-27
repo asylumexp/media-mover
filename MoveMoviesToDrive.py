bfb6f3213a24d32e9d60680f0d48cdbb609bd8fd
Sam Heinz <54530346+samh06@users.noreply.github.com>
2023-06-27 15:15:17 +1000

    Code ripped from stackoverflow
    shutil is very slow
    
    https://stackoverflow.com/questions/22078621/how-to-copy-files-fast
    
    + chatgpt

--- MoveMoviesToDrive.py
+++ MoveMoviesToDrive.py
@@ -1,115 +1,86 @@
 import os
-import shutil
-import keyboard
 from tqdm import tqdm
-import sys
-import logging
-import datetime
-
-# Create a logger
-logger = logging.getLogger("my_logger")
-logger.setLevel(logging.DEBUG)
-
-# Create a formatter for the log messages
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-
-# Create a console handler
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.ERROR)  # Set the log level to ERROR or higher
-console_handler.setFormatter(formatter)
-
-# Create a file handler
-current_datetime = datetime.datetime.now()
-log_file_name = current_datetime.strftime("%Y-%m-%d-%H-%M-log.log")
-log_file_path = rf"C:\users\wheel\Documents\logs\{log_file_name}"
-file_handler = logging.FileHandler(log_file_path)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+import shutil
 
 
-def count_files(source_folder):
-    total_files = 0
-    for root, dirs, files in os.walk(source_folder):
-        total_files += len(files)
-    return total_files
+class CTError(Exception):
+    def __init__(self, errors):
+        self.errors = errors
 
 
-def copy_folders(source_drive, destination_drive):
-    source_folder = source_drive
-    destination_folder = destination_drive
+try:
+    O_BINARY = os.O_BINARY
+except:
+    O_BINARY = 0
+READ_FLAGS = os.O_RDONLY | O_BINARY
+WRITE_FLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC | O_BINARY
+BUFFER_SIZE = 128*1024
 
+
+def copyfile(src, dst):
     try:
-        # Create the destination folder if it doesn't exist
-        os.makedirs(destination_folder, exist_ok=True)
+        # Check if file already exists in destination with the same size
+        if os.path.exists(dst) and os.path.getsize(src) == os.path.getsize(dst):
+            return
 
-        total_files = count_files(source_folder)
-
-        with tqdm(total=total_files, desc="Copying files", unit="files") as pbar:
-            for root, dirs, files in os.walk(source_folder):
-                # Copy directories
-                for dir in dirs:
-                    source_path = os.path.join(root, dir)
-                    relative_path = os.path.relpath(source_path, source_folder)
-                    destination_path = os.path.join(
-                        destination_folder, relative_path)
-
-                    os.makedirs(destination_path, exist_ok=True)
-
-                # Copy files
-                for file in files:
-                    source_file = os.path.join(root, file)
-                    relative_file = os.path.relpath(source_file, source_folder)
-                    destination_file = os.path.join(
-                        destination_folder, relative_file)
-
-                    try:
-                        pbar.update(1)  # Increment the progress bar
-                        pbar.set_postfix(
-                            {"Current file": os.path.basename(source_file)})
-
-                        if keyboard.is_pressed('esc'):
-                            logger.info("File transfer stopped by user.")
-                            print("File transfer stopped by user.")
-                            sys.exit()
-
-                        if os.path.exists(destination_file) and os.path.getsize(rf"{source_file}") != os.path.getsize(rf"{destination_file}"):
-                            logger.info(
-                                f"Replacing file: {os.path.basename(source_file)}")
-                            shutil.copy2(source_file, destination_file)
-                            logger.info("Successful transfer.")
-                        if not os.path.exists(rf"{destination_file}"):
-                            logger.info(
-                                f"Current file: {os.path.basename(source_file)}")
-                            shutil.copy2(source_file, destination_file)
-                            logger.info("Successful transfer.")
-                        else:
-                            logger.info("File Exists.")
-
-                    except Exception:
-                        logger.info("Skipped transfer.")
-
-        logger.info("All folders and files copied successfully!")
-        print("All folders and files copied successfully!")
-
-    except Exception as e:
-        logger.error(f"An error occurred during the copy process: {str(e)}")
-        print(f"An error occurred during the copy process: {str(e)}")
-
-# Example usage
+        fin = os.open(src, READ_FLAGS)
+        stat = os.fstat(fin)
+        fout = os.open(dst, WRITE_FLAGS, stat.st_mode)
+        total_size = os.path.getsize(src)
+        progress_bar = tqdm(total=total_size, unit='B',
+                            unit_scale=True, desc=os.path.basename(src))
+        for x in iter(lambda: os.read(fin, BUFFER_SIZE), b""):
+            os.write(fout, x)
+            progress_bar.update(len(x))
+        shutil.copystat(src, dst)
+        progress_bar.close()
+    finally:
+        try:
+            os.close(fin)
+        except:
+            pass
+        try:
+            os.close(fout)
+        except:
+            pass
 
 
-source_drive = r'E:\Media\TV Shows'
-destination_drive = r'D:\Media\TV'
-copy_folders(source_drive, destination_drive)
+def copytree(src, dst, symlinks=False, ignore=[]):
+    names = os.listdir(src)
 
-source_drive = r'E:\Media\Anime'
-destination_drive = r'D:\Media\Anime'
-copy_folders(source_drive, destination_drive)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignore:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                copyfile(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error) as why:
+            errors.append((srcname, dstname, str(why)))
+        except CTError as err:
+            errors.extend(err.errors)
+    if errors:
+        raise CTError(errors)
 
-source_drive = r'E:\Media\Movies'
-destination_drive = r'D:\Media\Movies'
-copy_folders(source_drive, destination_drive)
-sys.stdout.close()
+
+src_directory = 'E:\\Media\\Anime'
+dst_directory = 'D:\\Media\\Anime'
+copytree(src_directory, dst_directory, symlinks=False, ignore=[])
+
+src_directory = 'E:\\Media\\TV Shows'
+dst_directory = 'D:\\Media\\TV'
+copytree(src_directory, dst_directory, symlinks=False, ignore=[])
+
+src_directory = 'E:\\Media\\Movies'
+dst_directory = 'D:\\Media\\Movies'
+copytree(src_directory, dst_directory, symlinks=False, ignore=[])
